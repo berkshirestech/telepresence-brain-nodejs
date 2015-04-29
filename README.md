ba917f884e6cf129fc585bde1b621fce30802a52
Joseph Method <tristil@gmail.com>
2015-04-29 00:35:38 -0400

    Simplified interface and added README
    Now the system that is issuing commands is responsible for issuing an explicit
    stop statement instead of hardcoding command durations.

---
+++ README.md
@@ -0,0 +1,31 @@
+# telepresence-brain-nodejs
+
+This is an application that will live on a Raspberry Pi attached to the body of
+a telepresence robot. The Raspberry Pi will be connected by serial connection
+to one or two Arduinos that will be used to control the robot's wheels and the
+camera movement. This application will control these two subsystems and
+communicate with a central webserver that will relay messages from a user
+directing the robot.
+
+**Note:** Right now this code is based off of code that was used to control a
+USB rocket launcher. In the future the rocket launcher related code will be
+removed in favor of code that interfaces with the camera swivel system.
+
+## Requirements
+
+- [Node.js](https://nodejs.org/)
+- [The web server](https://github.com/berkshirestech/telepresence-webserver)
+  (optional)
+- [Rocket launcher](http://dreamcheeky.com/thunder-missile-launcher) See note
+  above. This is temporary.
+
+## Usage
+
+``` bash
+# use your IP and port here
+SERVER_IP=192.168.1.24 SERVER_PORT=3001 node index.js
+```
+
+The script will connect as a client to a webserver running socket.io at the
+provided IP and port. If the server sends a socket.io message of 'keydown' the
+data for the message will be passed to the rocket launcher class.
--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 var socketIo = require('socket.io-client'),
-    RocketLauncher = require('./launcher').RocketLauncher;
+    RocketLauncher = require('./launcher').RocketLauncher,
+    serverIp = process.env.SERVER_IP,
+    serverPort = process.env.SERVER_PORT;
+
+if(!serverIp)
+  serverIp = '127.0.0.1'
+
+if(!serverPort)
+  serverPort = 3000
 
 rocketLauncher = new RocketLauncher();
 
-socket = socketIo('http://192.168.1.24:3000');
+socket = socketIo('http://' + serverIp +':3000');
 rocketLauncher.init();
-
-rocketLauncher.testFunctions();
 
 socket.on('connect',
   function(){
     console.log('connected');
-    socket.emit('imarobot', { named: 'Joe' });
+    socket.emit('imarobot');
   }
 );
 
@@ -23,40 +29,17 @@ socket.on('disconnect',
   function(){ console.log('disconnected') }
 );
 
-socket.on('keypress',
+var performAction = function(action) {
+  rocketLauncher[action]();
+  console.log('performed action ' + action);
+}
+
+socket.on('keydown',
   function(data){
-    switch(data) {
-      case 'shoot':
-        rocketLauncher.shoot();
-        setTimeout(
-          function() { rocketLauncher.boom() }, 8000
-          );
-        break;
-      case 'left':
-        rocketLauncher.left();
-        setTimeout(
-          function() { rocketLauncher.stop() }, 100
-        );
-        break;
-      case 'right':
-        rocketLauncher.right();
-        setTimeout(
-          function() { rocketLauncher.stop() }, 100
-        );
-        break;
-      case 'up':
-        rocketLauncher.up();
-        setTimeout(
-          function() { rocketLauncher.stop() }, 100
-        );
-        break;
-      case 'down':
-        rocketLauncher.down();
-        setTimeout(
-          function() { rocketLauncher.stop() }, 100
-        );
-        break;
-    }
+    performAction(data);
   }
 );
 
+socket.on('stop',
+  function(data) { rocketLauncher.stop() }
+);
